f14630bf4517b1ac25d5efada7acce0335bd0fc1
b-3-n <benjamin@handtracking.io>
2022-02-04 14:28:48 +0100

    Add debug output for docs ci action.

--- ci/ensure_clean_tree.sh
+++ ci/ensure_clean_tree.sh
@@ -2,10 +2,14 @@
 
 # Check for new files.
 if [[ -z $(git ls-files --exclude-standard --others) ]]; then
-  exit 1;
+  echo "Error: Code and docs are out of sync. Printing output of 'git status' for reference." >&2
+  git status
+  exit 1
 fi
 
 # Check for modified files.
 if [[ -z $(git diff-files --quiet) ]]; then
-  exit 1;
+  echo "Error: Code and docs are out of sync. Printing output of 'git status' for reference." >&2
+  git status
+  exit 1
 fi
