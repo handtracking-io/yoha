fbb446b5e24b4ee1caeef11cf0a24a5777a4e1aa
b-3-n <benjamin@handtracking.io>
2022-02-04 14:40:40 +0100

    Fix bug in ci script.

--- ci/ensure_clean_tree.sh
+++ ci/ensure_clean_tree.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Check for new files.
-if [[ -z $(git ls-files --exclude-standard --others) ]]; then
+if [[ $(git ls-files --exclude-standard --others) ]]; then
   echo "Error: Code and docs are out of sync. Printing output of 'git status' for reference." >&2
   git status
   exit 1
 fi
 
 # Check for modified files.
-if [[ -z $(git diff-files --quiet) ]]; then
+if [[ $(git diff-files --quiet) ]]; then
   echo "Error: Code and docs are out of sync. Printing output of 'git status' for reference." >&2
   git status
   exit 1
